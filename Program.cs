32545bdf3e2ccdf19dbc98c0ef47aa722079c271
Филиппов Алексей <teenwizard.fornewvideo@gmail.com>
2024-04-13 14:22:22 +0300

    deletingdirectory

--- Program.cs
+++ Program.cs
@@ -9,11 +9,34 @@
 
         static void GetCatalogsCount()
         {
-            string dirName = @"C:/"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            string dirName = @"/"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
             if (Directory.Exists(dirName)) // Проверим, что директория существует
             {
                 Console.WriteLine("Папки:");
                 string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+                Console.WriteLine(dirs.Length);
+
+                DirectoryInfo newDirectory = new DirectoryInfo(@"/newDirectory");
+                if (!newDirectory.Exists)
+                    newDirectory.Create();
+
+                Console.WriteLine("Папки:");
+                dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+                Console.WriteLine(dirs.Length);
+
+                try
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(@"/newDirectory");
+                    dirInfo.Delete(true); // Удаление со всем содержимым
+                    Console.WriteLine("Каталог удален");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("Папки:");
+                dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
                 Console.WriteLine(dirs.Length);
 
                 Console.WriteLine();
